77e01cd61c386c3234aa7263fe2db36ecc651143
Alexander Gray <alexander@ultamatum.dev>
2022-06-05 21:33:04 +0100

    Separated commands to their own folder

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,21 +1,24 @@
 require('dotenv').config()
+const fs = require('node:fs')
+const path = require('node:path')
 const {
 	SlashCommandBuilder,
 } = require('@discordjs/builders')
 const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
 
-const commands = [
-	new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with pong!'),
-	new SlashCommandBuilder()
-		.setName('server')
-		.setDescription('Replies with server info!'),
-	new SlashCommandBuilder()
-		.setName('user')
-		.setDescription('Replies with user info!'),
-].map((command) => command.toJSON())
+const commands = []
+
+const commandsPath = path.join(__dirname, 'commands')
+const commandFiles = fs
+	.readdirSync(commandsPath)
+	.filter((file) => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file)
+	const command = require(filePath)
+	commands.push(command.data.toJSON())
+}
 
 const rest = new REST({ version: '9' }).setToken(
 	process.env.DISCORD_TOKEN
--- src/main.js
+++ src/main.js
@@ -1,33 +1,53 @@
 require('dotenv').config()
+const fs = require('node:fs')
+const path = require('node:path')
 const {
 	Client,
 	Intents,
-	Interaction,
+	Collection,
 } = require('discord.js')
 
 const client = new Client({
 	intents: [Intents.FLAGS.GUILDS],
 })
 
-client.on('ready', () => {
+client.commands = new Collection()
+const commandsPath = path.join(__dirname, 'commands')
+const commandFiles = fs
+	.readdirSync(commandsPath)
+	.filter((file) => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file)
+	const command = require(filePath)
+
+	// Set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command)
+}
+
+client.once('ready', () => {
 	console.log(`Logged in as ${client.user.tag}`)
 })
 
 client.on('interactionCreate', async (interaction) => {
 	if (!interaction.isCommand()) return
 
-	const { commandName } = interaction
+	const command = client.commands.get(
+		interaction.commandName
+	)
 
-	if (commandName === 'ping') {
-		await interaction.reply('pong!')
-	} else if (commandName === 'server') {
-		await interaction.reply(
-			`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
-		)
-	} else if (commandName === 'user') {
-		await interaction.reply(
-			`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
-		)
+	if (!command) return
+
+	try {
+		await command.execute(interaction)
+	} catch (error) {
+		console.error(error)
+		await interaction.reply({
+			content:
+				'There was an error while executing this command!',
+			ephemeral: true,
+		})
 	}
 })
 
